4e7a6283b672954314b722e3865d2de1839572ab
Lord-Einstein <darylleinstein@gmail.com>
2025-11-26 23:54:17 +0100

    Fix : Genarator functions.

--- 003-php-password-generator/app/index.php
+++ 003-php-password-generator/app/index.php
@@ -1,24 +1,29 @@
 <?php
 
-$options = "";
-$placeholder = "";
+$html = "";
 
-$default_selected = 10;
+$options = "";
+//$placeholder = "";
+
+//$default_selected = 10;
 
 $maj_letters = 0;
 $min_letters = 0;
 $numbers = 0;
 $symbols = 0;
 
+$check_state_maj_letters = "checked";
+$check_state_min_letters = "checked";
+$check_state_numbers = "checked";
+$check_state_symbols = "checked";
 
 $passwordGenerated = "";
+$selected_size = 10;
 
-$selected_size = $default_selected;
-
-$options = generateSelectOptions($placeholder);
+$options = generateSelectOptions($selected_size);
 
 function generateRandomString(array $chars, int $length = 1): string {
-    $str = '';
+    $str = "";
     $maxIndex = count($chars) - 1;
 
     for ($i = 0; $i < $length; $i++) {
@@ -33,7 +38,7 @@ function generatePassword(int $maj_letters, int $min_letters, int $numbers, int 
     $maj_array = range('A', 'Z');
     $min_array = range('a', 'z');
     $num_array = range(0, 9);
-    $symbols_array = str_split("!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ");
+    $symbols_array = str_split("!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
 
     $coched = 0;
     $mix_array = [];
@@ -41,24 +46,29 @@ function generatePassword(int $maj_letters, int $min_letters, int $numbers, int 
     $password = "";
 
     if($maj_letters){
-        $mix_array = array_merge($maj_array);
+        $mix_array = array_merge($mix_array, $maj_array);
         $password .= generateRandomString($maj_array);
         $coched++;
     }if($min_letters){
-        $mix_array = array_merge($min_array);
+        $mix_array = array_merge($mix_array, $min_array);
         $password .= generateRandomString($min_array);
         $coched++;
     }if($numbers){
-        $mix_array = array_merge($num_array);
+        $mix_array = array_merge($mix_array, $num_array);
         $password .= generateRandomString($num_array);
         $coched++;
     }if($symbols){
-        $mix_array = array_merge($symbols_array);
+        $mix_array = array_merge($mix_array, $symbols_array);
         $password .= generateRandomString($symbols_array);
         $coched++;
     }
 
-    $password .= generateRandomString($mix_array, ($selected_size - $coched ));
+    if($coched === 0) {
+        return "";
+    }
+
+    $selected_size -= $coched;
+    $password .= generateRandomString($mix_array, $selected_size);
     $password = str_shuffle($password);
 
 //    if($coched === 0) {$password = "";}
@@ -66,7 +76,8 @@ function generatePassword(int $maj_letters, int $min_letters, int $numbers, int 
 }
 
 
-function generateSelectOptions (string &$placeholder, int &$default_selected = 10, int $min_size = 8, int $max_size = 45) : string {
+//function generateSelectOptions (string &$placeholder, int $default_selected = 10, int $min_size = 8, int $max_size = 45) : string {
+function generateSelectOptions (int $default_selected = 10, int $min_size = 8, int $max_size = 43) : string {
     $options = "";
     if($default_selected < $min_size){$default_selected = $min_size;}
     for($i = $min_size; $i <= $max_size; $i++) {
@@ -77,7 +88,7 @@ function generateSelectOptions (string &$placeholder, int &$default_selected = 1
         }
         $options .= $option;
     }
-    $placeholder = str_repeat("*", $default_selected);
+//    $placeholder = str_repeat("*", $default_selected);
     return $options;
 }
 
@@ -85,27 +96,39 @@ function generateSelectOptions (string &$placeholder, int &$default_selected = 1
 
 if(isset($_POST["submit"])){
 
-    $maj_letters = (int) $_POST["maj_letters"] ?? 0;
-    $min_letters = (int) $_POST["min_letters"] ?? 0;
-    $numbers = (int) $_POST["numbers"] ?? 0;
-    $symbols = (int) $_POST["symbols"] ?? 0;
+    $maj_letters = $_POST["maj_letters"] ?? 0;
+    $min_letters = $_POST["min_letters"] ?? 0;
+    $numbers = $_POST["numbers"] ?? 0;
+    $symbols = $_POST["symbols"] ?? 0;
 
-    $selected_size = (int) $_POST["size"] ?? 0;
+    $selected_size = ($_POST["size"]) ?? 0;
+
+    $maj_letters = (int)$maj_letters;
+    $min_letters = (int)$min_letters;
+    $numbers = (int)$numbers;
+    $symbols = (int)$symbols;
+
+    $check_state_maj_letters = $maj_letters ? "checked" : "";
+    $check_state_min_letters = $min_letters ? "checked" : "";
+    $check_state_numbers = $numbers ? "checked" : "";
+    $check_state_symbols = $symbols ? "checked" : "";
+
+    $selected_size = (int)$selected_size;
+
 
     $passwordGenerated = generatePassword($maj_letters, $min_letters, $numbers, $symbols, $selected_size);
+    $options = generateSelectOptions($selected_size);
 
 
 
 } else {
-    return;
+    echo "VIDE \n";
 }
 
-$options = generateSelectOptions($placeholder);
 
 
 
 $html = <<<HTML
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -121,10 +144,10 @@ $html = <<<HTML
                 <legend>Password Generator.</legend>
 
                 <label for="password">Password.</label>
-                <input type="text" name="password" id="password" value="" placeholder="$placeholder" readonly>
+                <input type="text"  id="password" value="{$passwordGenerated}" readonly>
             </fieldset>
 
-            <button type="submit">Genarate.</button>
+            <button type="submit" name="submit">Genarate.</button>
             
              <fieldset>
                 <label for="size">Generated text size.</label>
@@ -136,34 +159,21 @@ $html = <<<HTML
             <div class="conditions">
                 <fieldset>
                     <label for="maj_letters">Inclure les lettres majuscules.</label>
-                    <input type="checkbox" name="maj_letters" id="maj_letters" value="1" checked>
+                    <input type="checkbox" name="maj_letters" id="maj_letters" value="1" $check_state_maj_letters>
                 </fieldset>
                 <fieldset>
                     <label for="min_letters">Inclure les lettres minuscules.</label>
-                    <input type="checkbox" name="min_letters" id="min_letters" value="1" checked>
+                    <input type="checkbox" name="min_letters" id="min_letters" value="1" $check_state_min_letters>
                 </fieldset>
                 <fieldset>
                     <label for="numbers">Inclure les chiffres de 0 à 9.</label>
-                    <input type="checkbox" name="numbers" id="numbers" value="1" checked>
+                    <input type="checkbox" name="numbers" id="numbers" value="1" $check_state_numbers>
                 </fieldset>
                 <fieldset>
                     <label for="symbols">Inclure les symboles (%>#...).</label>
-                    <input type="checkbox" name="symbols" id="symbols" value="1" checked>
+                    <input type="checkbox" name="symbols" id="symbols" value="1" $check_state_symbols>
                 </fieldset>
             </div>
-        
-
-HTML;
-
-
-
-
-
-
-
-
-
-$html .= <<<HTML
         </form>
     </main>
 </body>
