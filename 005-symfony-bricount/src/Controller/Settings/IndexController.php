98e8b7fdae1880538f917f99e9be2d7e560282fe
Lord-Einstein <darylleinstein@gmail.com>
2025-12-25 11:07:35 +0100

    feat : Customize all styles step 2.

--- 005-symfony-bricount/src/Controller/Settings/IndexController.php
+++ 005-symfony-bricount/src/Controller/Settings/IndexController.php
@@ -56,11 +56,48 @@ final class IndexController extends AbstractController
             'https://cdn3d.iconscout.com/3d/premium/thumb/girl-avatar-6299534-5187866.png',
             'https://cdn3d.iconscout.com/3d/premium/thumb/black-man-avatar-6299542-5187874.png',
             'https://cdn3d.iconscout.com/3d/premium/thumb/punk-man-avatar-6299537-5187869.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/curly-hair-man-with-glasses-3d-icon-png-download-5823020.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/dreadlocked-man-3d-icon-png-download-5823057.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/curly-hair-man-3d-icon-png-download-5823051.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/man-with-glasses-3d-icon-png-download-5823041.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/short-hair-man-with-glasses-3d-icon-png-download-5823072.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/man-3d-icon-png-download-5823043.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/man-with-beard-3d-icon-png-download-5823044.png',
+
+            'https://cdn3d.iconscout.com/3d/premium/thumb/ronin-red-samurai-suits-3d-icon-png-download-8382309.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/chinese-cat-in-samurai-suit-3d-icon-png-download-8382307.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/samurai-themed-kubricks-samurai-suits-3d-icon-png-download-8382304.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/sengoku-basara-nendoroids-samurai-suits-3d-icon-png-download-8382301.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/tamashii-nations-figuarts-samurai-suits-3d-icon-png-download-8382302.png',
+
+            'https://cdn3d.iconscout.com/3d/premium/thumb/ramen-3d-icon-png-download-4847193.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/takoyaki-3d-icon-png-download-8436638.png',
+
         ];
 
         $womenAvatars = [
-            'https://cdn3d.iconscout.com/3d/premium/thumb/black-woman-avatar-6299541-5187873.png',
-            'https://cdn3d.iconscout.com/3d/premium/thumb/girl-avatar-6299534-5187866.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/colombian-3d-icon-png-download-5251135.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/with-glasses-3d-icon-png-download-5823066.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/woman-with-hijab-3d-icon-png-download-5823033.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/woman-with-hijab-and-glasses-3d-icon-png-download-5823034.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-with-hat-3d-icon-png-download-5823023.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-with-hat-3d-icon-png-download-5823025.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-with-hat-3d-icon-png-download-5823029.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-with-glasses-3d-icon-png-download-5823031.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-3d-icon-png-download-5823027.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-3d-icon-png-download-5823024.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-3d-icon-png-download-5823022.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/female-avatars-3d-icon-png-download-10764486.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/long-hair-woman-with-glasses-3d-icon-png-download-5823021.png',
+
+            'https://cdn3d.iconscout.com/3d/premium/thumb/ronin-red-samurai-suits-3d-icon-png-download-8382309.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/chinese-cat-in-samurai-suit-3d-icon-png-download-8382307.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/samurai-themed-kubricks-samurai-suits-3d-icon-png-download-8382304.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/sengoku-basara-nendoroids-samurai-suits-3d-icon-png-download-8382301.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/tamashii-nations-figuarts-samurai-suits-3d-icon-png-download-8382302.png',
+
+            'https://cdn3d.iconscout.com/3d/premium/thumb/ramen-3d-icon-png-download-4847193.png',
+            'https://cdn3d.iconscout.com/3d/premium/thumb/takoyaki-3d-icon-png-download-8436638.png',
         ];
 
         $avatars = $user->getGender() == 'M' ? $menAvatars : $womenAvatars;
